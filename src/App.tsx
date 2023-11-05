1cffe059dfab6e2c4a0b5d51952f93c0514fe32a
ardhikaxx <ardhikayanuar58@gmail.com>
2023-11-06 00:28:00 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const handleShowToast = () => {
+    toast.success('Ini adalah pesan toast success.', {
+      position: 'top-right',
+      autoClose: 3000,
+    });
+
+    toast.info('Ini adalah pesan toast info.', {
+      position: 'top-right',
+      autoClose: 3000,
+    });
+
+    toast.warning('Ini adalah pesan toast warning.', {
+      position: 'top-right',
+      autoClose: 3000,
+    });
+
+    toast.error('Ini adalah pesan toast error (danger).', {
+      position: 'top-right',
+      autoClose: 3000,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fitur Toast</h1>
+        <button className="show-toast-button" onClick={handleShowToast}>
+          Tampilkan Toast
+        </button>
+        <ToastContainer />
       </header>
     </div>
   );
